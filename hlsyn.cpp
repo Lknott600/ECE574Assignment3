1c05692b11c4c6eb9f39c5a228da4f2450c4ff34
brencass20 <33562885+brencass20@users.noreply.github.com>
2018-11-13 10:18:15 -0700

    Create hlsyn.cpp

---
+++ hlsyn.cpp
@@ -0,0 +1,8 @@
+/*
+Name: Brendan Cassidy, Diego Alcantra, Evan Weiler, Logan Knott
+Project: ECE 474/574 Project 3
+Description:
+This project creates an implementation of the Force Directed Schedule taking in the following 
+format for command line args:
+hlsyn cFile latency(int) verilogFile(output.v)
+*/
