7bb3b7e6d6b1c5d4965697276b8dce250d57f0d3
brencass20 <33562885+brencass20@users.noreply.github.com>
2018-11-14 10:40:26 -0700

    Changed formatting
    Simplifying main file

--- hlsyn.cpp
+++ hlsyn.cpp
@@ -2,7 +2,37 @@
 Name: Brendan Cassidy, Diego Alcantra, Evan Weiler, Logan Knott
 Project: ECE 474/574 Project 3
 Description:
-This project creates an implementation of the Force Directed Schedule taking in the following 
+This project creates an implementation of the Force Directed Schedule taking in the following
 format for command line args:
 hlsyn cFile latency(int) verilogFile(output.v)
 */
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include "Variable.hpp"
+#include "functionsIO.hpp"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	//if (argc != 3) {
+	//	cout << "Usage: main inputFile outputFile" << endl;
+	//	return EXIT_FAILURE;
+	//}
+
+	vector<Variable> allVariables;
+	//IO, we must set up the writing of the variables to the outputs [Handled once AllVars returns]
+	//Create vector of all Variables [Done with allVariables]
+	//Map the Operations [Operation Class will handle this]
+	allVariables = inputFileToVariables(argv[1]);
+	
+	//Send to the force directed scheduling.
+
+	//writing to output file
+	outputFileCreate(allVariables, argv[2]);
+	
+	return 0;
+};
