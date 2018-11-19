c64b41a8f19ba957144104df6ed6aad8bc2ac279
brencass20 <33562885+brencass20@users.noreply.github.com>
2018-11-18 17:25:52 -0700

    Update functionsIO.hpp

--- functionsIO.hpp
+++ functionsIO.hpp
@@ -21,11 +21,11 @@ vector<Variable> inputFileToVariables(string fileName, vector<Operation> *allOps
 vector<Variable> compileListOfVariables(string line, vector<Variable> allVariables,	string currType, 
 	string bitWidth, string varNames, Variable tempVar, string delimiter, string currName,	int pos);
 
-vector<Operation> compileListOfOperations(string line, vector<Variable> allVariables, vector<Operation> allOperations,
+void compileListOfOperations(string line, vector<Variable> allVariables, vector<Operation> *allOperations,
 	vector<Variable> currOperand, string val, int count, bool validVar, bool signedFlag,
-	int maxDatawidth, int flagExtendLocVar1, int flagExtendLocVar2, int operandCount,
-	bool flagIncDec);
+	int maxDatawidth, int operandCount,	bool flagIncDec);
 
+void dependentOperation(Operation *currOperation, vector<Operation> *allOperations);
 
 //File Output
 void outputFileCreate(vector<Variable> allVariables, string outFile);
